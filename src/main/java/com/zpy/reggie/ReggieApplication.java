9747696571a87ab131ca27590cadbf0b3912f305
charles <1021656349@qq.com>
2023-07-08 19:57:55 +0800

    使用Spring Cache来缓存套餐数据

--- src/main/java/com/zpy/reggie/ReggieApplication.java
+++ src/main/java/com/zpy/reggie/ReggieApplication.java
@@ -4,11 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
-
+@EnableCaching
 public class ReggieApplication {
 
     public static void main(String[] args) {
